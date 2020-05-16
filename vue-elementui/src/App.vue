d3e0864e81328cfd305c7219c2611a73930b19b7
loonmucy <1540259636@qq.com>
2020-05-16 14:05:17 +0800

    添加系列表格功能

--- vue-elementui/src/App.vue
+++ vue-elementui/src/App.vue
@@ -1,15 +1,45 @@
 <template>
-  <div id="app">
-    <el-button type="primary">主要按钮</el-button>
-  <el-button type="success">成功按钮</el-button>
+  <div id="app" class="my-app">
+    <div style="flex:1;margin-right:10px">
+      <el-table ref="detailTable" class="atb-table" border :show-header="false" size="mini" height="100%" :data="list" highlight-current-row @current-change="currentChangeHandle">
+          <el-table-column label="table仿造导航" prop="prop0"></el-table-column>
+      </el-table>
+    </div>
+    <div style="flex:3">
+        <component :is="currentView"></component>
+    </div>
   </div>
 </template>
 
 <script>
+import checkboxAddTable from './components/checkboxAddTable'
+import dynamicForm from './components/dynamicForm'
+import tableRowClass from './components/tableRowClass'
+import dynamicAddRow from './components/dynamicAddRow'
+import copyNavigation from './components/copyNavigation'
 
 export default {
   name: 'App',
   components: {
+    checkboxAddTable,dynamicForm,tableRowClass,dynamicAddRow,copyNavigation
+  },
+  data() {
+    return {
+      list: [
+        {'prop0':'表格动态列','name':'checkboxAddTable'},
+        {'prop0':'表格动态行列','name':'dynamicForm'},
+        {'prop0':'表单行样式','name':'tableRowClass'},
+        {'prop0':'动态添加表格行','name':'dynamicAddRow'},
+        {'prop0':'表格仿造导航功能','name':'copyNavigation'},
+      ],
+      currentView: 'checkboxAddTable'
+    }
+  },
+  methods: {
+    currentChangeHandle(row) {
+      let label = row.name
+      this.currentView = label
+    },
   }
 }
 </script>
@@ -22,5 +52,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+.my-app {
+  display: flex;
+  /* justify-content: column;; */
 }
 </style>
---
+++ vue-elementui/src/components/copyNavigation.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="atb-card split-form">
+            <el-form>
+                <el-row class="alignText">
+                    <span>表格仿造导航功能</span>
+                </el-row>
+            </el-form>
+            <div style="display:flex">
+                <div style="flex:1;margin-right:10px">
+                    <el-table ref="detailTable1" class="atb-table" border size="mini" height="100%" :data="list1" highlight-current-row @current-change="currentChangeHandle">
+                        <el-table-column label="仿造导航" prop="prop0"></el-table-column>
+                    </el-table>
+                </div>
+                <div style="flex:3">
+                    <el-table ref="detailTable" class="atb-table" border size="mini" height="100%" :data="list2">
+                        <el-table-column label="第一列" prop="prop0"></el-table-column>
+                        <el-table-column label="第二列" prop="prop1"></el-table-column>
+                    </el-table>
+                </div>
+            </div>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            list1:[
+                {'prop0':'导航1'},
+                {'prop0':'导航2'},
+                {'prop0':'导航3'},
+                {'prop0':'导航4'},
+                {'prop0':'导航5'},
+                {'prop0':'导航6'},
+            ],
+            list2:[]
+        }
+    },
+    created() {
+        this.$nextTick(() => {
+            this.$refs.detailTable1.setCurrentRow(this.list1[0])
+        })
+    },
+    methods: {
+        currentChangeHandle(row) {
+            this.list2 = []
+            let label = row.prop0
+            this.list2.push(
+                {'prop0':label+'11','prop1':label+'12'},
+                {'prop0':label+'21','prop1':label+'22'},
+                {'prop0':label+'31','prop1':label+'32'},
+
+            )
+        }
+    },
+}
+</script>
+<style scoped>
+
+</style>
