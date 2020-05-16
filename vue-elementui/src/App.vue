46dd9a831fc4b6e445bc4094fd51e2a6642fbf83
loonmucy <1540259636@qq.com>
2020-05-16 11:00:47 +0800

    项目初始化，引入element ui

---
+++ vue-elementui/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <el-button type="primary">主要按钮</el-button>
+  <el-button type="success">成功按钮</el-button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
